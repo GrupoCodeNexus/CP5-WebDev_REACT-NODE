af96a538df70cfea0d39a5caba4b1a0c3cdcd265
Kayque <kayquecarvalho020@gmail.com>
2025-05-08 11:22:38 -0300

    Feat: Install Tailwindcss

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,14 @@
+import './index.css'
 
 function App() {
 
 
   return (
     <>
+    {/* teste */}
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
     </>
   )
 }
