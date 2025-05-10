ac9174d4c7ee907dfe5fc818d390622d39f759e0
Matheus <m.eiki.ikeda@gmail.com>
2025-05-10 18:03:44 -0300

    Fix: Arrumando O App.jsx e o Error.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,19 +7,17 @@ import Home from '../Routes/Home'
 
 function App() {
 
-
-  return (
-    <div className='mr-6 ml-6 sm:mr-5 sm:ml-5 md:mr-10 md:ml-10'>
-    <Router>
-      <Header/>
-      <Routes>
-        <Route path="*" element={<Error/>}/>
-        <Route path="/" element={<Home/>}/>
-      </Routes>
-      <Footer/>
-    </Router>
-    </div>
-  )
+return ( <div className='mr-6 ml-6 sm:mr-5 sm:ml-5 md:mr-10 md:ml-10'> 
+<Router> 
+  <Header/> 
+    <Routes>
+      <Route path="*" element={<Error/>}/>
+      <Route path="/" element={<Home/>}/> 
+    </Routes> 
+  <Footer/> 
+</Router> 
+</div>
+)
 }
 
 export default App
