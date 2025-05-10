3ec4b117ea66f44ac98e50f7622181487b4fed9a
Matheus <m.eiki.ikeda@gmail.com>
2025-05-10 16:40:06 -0300

    Feat:Criando o Error e Fix:Arrumando o Error

--- frontend/Components/Header.jsx
+++ frontend/Components/Header.jsx
@@ -4,13 +4,10 @@ import logo from '../assets/logo.jpeg'
 const Header = () => (
   <header>
     <div className="container mx-auto flex items-center justify-between px-6 py-4">
-      
-      {/* Logo à esquerda */}
+
       <img src={logo} className="w-20 p-2" alt="Logo" />
       
-      {/* Navegação + Botão juntos */}
       <div className="flex items-center space-x-6">
-        {/* Navegação */}
         <ul className="flex flex-row text-blue-700 space-x-4">
           <li>
             <a href="#" className="transition duration-300 hover:text-blue-900">Área do Colaborador</a>
@@ -20,7 +17,6 @@ const Header = () => (
           </li>
         </ul>
 
-        {/* Botão */}
         <button className="cursor-pointer rounded-lg bg-yellow-500 px-4 py-2 transition duration-300 ease-in-out hover:scale-110 text-blue-800">
           Falar com o especialista
         </button>
