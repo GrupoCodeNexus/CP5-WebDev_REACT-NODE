0de7252e4f1c52448d4ebbf7bccbced9bf0db8be
Kayque <kayquecarvalho020@gmail.com>
2025-05-11 02:00:03 -0300

    Feat: Adicionando contéudo da CadastroHeader

--- frontend/Components/CadastroHeader.jsx
+++ frontend/Components/CadastroHeader.jsx
@@ -1,9 +1,16 @@
+import logo from '../assets/logo.jpeg'
+import { Link } from 'react-router-dom'
 
 const CadastroHeader = () => {
   return (
-    <div>
-      
-    </div>
+    <header>
+        <div className='container flex flex-row items-center justify-between md:justify-normal'>
+            <img src={logo} alt="Logo CodeNexus" className='w-20 p-2'/>
+            <div className='flex-initial ml-8'>
+                <Link to="/" className='text-blue-700 hover:text-blue-800 '>Voltar</Link>
+            </div>
+        </div>
+    </header>
   )
 }
 
