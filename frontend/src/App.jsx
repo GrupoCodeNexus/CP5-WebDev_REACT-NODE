7e3aef045967d65e5cf4acf6077ae1f69c300a7a
Kayque <kayquecarvalho020@gmail.com>
2025-05-11 00:20:05 -0300

    Feat: Criando rota para acessar página de Cadastros

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import Header from '../Components/Header'
 import Footer from '../Components/Footer'
 import Error from '../Routes/Error'
 import Home from '../Routes/Home'
+import Cadastros from '../Routes/Cadastros'
 
 function App() {
 
@@ -13,6 +14,7 @@ return ( <div className='mr-6 ml-6 sm:mr-5 sm:ml-5 md:mr-10 md:ml-10'>
     <Routes>
       <Route path="*" element={<Error/>}/>
       <Route path="/" element={<Home/>}/> 
+      <Route path="cadastrarProduto" element={<Cadastros/>}/>
     </Routes> 
   <Footer/> 
 </Router> 
